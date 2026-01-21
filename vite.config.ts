4237a96646ab243a30758e11dc3db583edadc8cb
Alex <abr2504@wgu.edu>
2026-01-20 21:30:32 -0800

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/HauntedMoodGarden/', // Set base for GitHub Pages
+  base: '/SpookyMoodGarden/', // Set base for GitHub Pages
   plugins: [react()],
   build: {
     outDir: 'docs'
